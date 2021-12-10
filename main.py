dfa296a21c61e732cb36e721f3b9db898c79dab0
roflanmen <bohdan7nava@gmail.com>
2021-12-10 22:30:17 +0200

    upd slowmode

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import json
 import telebot
 from threading import Timer
 from telebot.types import Poll
@@ -10,11 +11,11 @@ def votemute(message):
     user_id = message.reply_to_message.from_user.id
     user_name = message.reply_to_message.from_user.first_name
     poll = bot.send_poll(message.chat.id, "Mute " + user_name, ["+", "-"], is_anonymous=False)
-    t = Timer(45.0, checkPoll, [poll, message])
+    t = Timer(45.0, mutePoll, [poll, message])
     t.start()
 
 
-def checkPoll(poll, message):
+def mutePoll(poll, message):
     user = message.reply_to_message.from_user
     chat_id = poll.chat.id
     poll = bot.stop_poll(chat_id, poll.message_id)
@@ -25,7 +26,71 @@ def checkPoll(poll, message):
         bot.reply_to(message, "Muted for " + str(int(mute_time/60)) + " minutes")
     else:
         bot.reply_to(message, "Not enough votes")
+
+@bot.message_handler(commands=["slowmode"])
+def slowmode(message):
+    user_id = message.reply_to_message.from_user.id
+    user_name = message.reply_to_message.from_user.first_name
+    poll = bot.send_poll(message.chat.id, "Enable slowmode for " + user_name, ["+", "-"], is_anonymous=False)
+    t = Timer(15.0, slowModePoll, [poll, message])
+    t.start()
+
+
+def slowModePoll(poll, message):
+    user = message.reply_to_message.from_user
+    chat_id = poll.chat.id
+    poll = bot.stop_poll(chat_id, poll.message_id)
+    res = [poll.options[0].voter_count, poll.options[1].voter_count]
+    if sum(res) > 0 and res[0] > res[1]:
+        f = open('data.json')
+        data = json.load(f)
+        f.close()
+        data[user.id] = round(float(message.text.split()[-1])*60)
+        f = open('data.json', 'w')
+        json.dump(data, f)
+        f.close()
+        bot.reply_to(message, "Enabled slowmode(" + message.text.split()[-1] + " minutes)")
+    else:
+        bot.reply_to(message, "Not enough votes")
+
 # @bot.message_handler(commands=["votemute"])
-# @bot.message_handler(func=lambda message: True)
+    
+@bot.message_handler(commands=["unmute"])
+def unmute(message):
+    user_id = message.reply_to_message.from_user.id
+    user_name = message.reply_to_message.from_user.first_name
+    poll = bot.send_poll(message.chat.id, "Unmute " + user_name, ["+", "-"], is_anonymous=False)
+    t = Timer(15.0, unmutePoll, [poll, message])
+    t.start()
+
+def unmutePoll(poll, message):
+    user = message.reply_to_message.from_user
+    chat_id = poll.chat.id
+    poll = bot.stop_poll(chat_id, poll.message_id)
+    res = [poll.options[0].voter_count, poll.options[1].voter_count]
+    if sum(res) > 0 and res[0] > res[1]:
+        f = open('data.json')
+        data = json.load(f)
+        f.close()
+        del data[str(user.id)]
+        f = open('data.json', 'w')
+        json.dump(data, f)
+        f.close()
+        try:
+            bot.restrict_chat_member(chat_id, user.id, can_send_messages = True, can_send_media_messages = True, can_send_polls = True, can_send_other_messages = True)
+        except:
+            pass
+        bot.reply_to(message, "Unmuted")
+    else:
+        bot.reply_to(message, "Not enough votes")
+
+@bot.message_handler(func=lambda message: True)
+def applySlowMode(message):
+    f = open('data.json')
+    data = json.load(f)
+    f.close()
+    print(data.get(str(message.from_user.id), 0), str(message.from_user.id))
+    if(data.get(str(message.from_user.id), 0)):
+        bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date = round(time.time()) + data[str(message.from_user.id)], can_send_messages = False)
 
 bot.infinity_polling()
