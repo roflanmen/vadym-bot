d0b07bba6cf2f09e197b6f0748a25bce438b5adf
Bohdan Navalkovskyi <bohdan7nava@gmail.com>
2024-09-24 15:05:01 +0300

    Add simple endpoint

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from flask import Flask, request, send_from_directory
 app = Flask(__name__,
     static_url_path='', 
     static_folder='web')
+
+
+@app.route("/", methods=["GET"])
+def hello_world():
+    return "Hello, World!"
